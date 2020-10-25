07e1fda3f033883d8d2d109209f4df186980778b
donnay <38479794+donnayy@users.noreply.github.com>
2020-10-25 07:38:26 +0000

    Create br.lua

---
+++ locales/br.lua
@@ -0,0 +1,11 @@
+Locales['br'] = {
+	['blackmarket'] = 'comprar',
+	['blackmarket'] = 'blackmarket',
+	['press_menu'] = 'pressiona ~INPUT_CONTEXT~ para acessar blackmarket.',
+	['bought'] = 'tu compras-te ~y~%sx~s~ ~b~%s~s~ por ~r~€%s~s~',
+	['not_enough'] = 'não tens ~r~dinheiro suficiente~s~: %s',
+	['player_cannot_hold'] = 'tu ~r~não~s~ tens bastante ~y~espaço livre~s~ no teu inventário!',
+	['shop_confirm'] = 'comprar %sx %s por €%s?',
+	['no'] = 'não',
+	['yes'] = 'sim',
+}
